6d0171d7d931c440b819fc215da9b66e836a3756
ZJP <jinpeng.zhang@cotticoffee.com>
2024-04-03 16:40:48 +0800

    自动获取antd 组件的css

--- src/components/Comp2/index.tsx
+++ src/components/Comp2/index.tsx
@@ -1,9 +1,14 @@
+
+import { Button } from "antd";
 function Comp() {
   return (
     <div className="box">
       <p>222</p>
+      <Button type="primary" disabled>
+        Primary(disabled)
+      </Button>
     </div>
-  )
+  );
 }
 
 export default Comp;
